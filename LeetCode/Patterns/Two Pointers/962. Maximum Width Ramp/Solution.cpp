dc6b587031c5ea4e5a85f805d154b5e60fa42f6b
YugBairai <yugbairai200@gmail.com>
2026-08-20 10:16:12 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/962. Maximum Width Ramp/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/962. Maximum Width Ramp/Solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
 
-        int ans = INT_M;
+        int ans = INT_MIN;
 
         for (int i = 0; i < size(nums); i++) {
             for (int j = i + 1; j < size(nums); j++) {
