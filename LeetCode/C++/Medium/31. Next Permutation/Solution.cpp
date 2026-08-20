6cc67c20ae6b9cc9fb42e7b0ad7f170363d16852
YugBairai <yugbairai200@gmail.com>
2026-08-20 11:58:29 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/31. Next Permutation/Solution.cpp
+++ LeetCode/C++/Medium/31. Next Permutation/Solution.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        
+        int idx=-1;
+        int n = size(nums);
 
-        int n = nums.size();
-        int idx = -1;
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
                 idx = i;
                 break;
             }
         }
 
-        // No next permutation
         if (idx == -1) {
             reverse(nums.begin(), nums.end());
             return;
         }
 
-        // 2. Find element just greater than pivot
         for (int i = n - 1; i > idx; i--) {
             if (nums[i] > nums[idx]) {
                 swap(nums[i], nums[idx]);
@@ -26,7 +24,8 @@ public:
             }
         }
 
-        // 3. Reverse the suffix
         reverse(nums.begin() + idx + 1, nums.end());
+
+        
     }
 };
