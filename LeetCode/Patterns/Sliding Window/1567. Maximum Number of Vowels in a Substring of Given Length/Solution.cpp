18d472f4d6066550945c35332245c9d8d4bedeaa
YugBairai <yugbairai200@gmail.com>
2026-08-26 10:58:15 +0530

    Add Medium C++ Solution for 1567. Maximum Number of Vowels in a Substring of Given Length from LeetCode

--- LeetCode/Patterns/Sliding Window/1567. Maximum Number of Vowels in a Substring of Given Length/Solution.cpp
+++ LeetCode/Patterns/Sliding Window/1567. Maximum Number of Vowels in a Substring of Given Length/Solution.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int i = 0, j=k, p=0,q=s.size(),ss=i;
+        int i = 0, j = k, p = 0, q = s.size(), ss = i;
 
-        int maxi=0;
-        while(p<q){
-        string tmp ="";
+        int maxi = 0;
+        while (p <= q-k) {
+            string tmp = "";
 
-        while(i<k){
-            tmp += s[i];
-            i++;
-        }
-
-        int v=0;
-
-        for(char c : tmp){
-            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                v++;
+            while (i < k) {
+                tmp += s[i];
+                i++;
             }
-        }
 
-        if(v==k) return k;
+            int v = 0;
 
-        maxi = max(v,maxi);
+            for (char c : tmp) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    v++;
+                }
+            }
 
-        p++;
-        i = ss+1;
-        ss = i;
-        k++;
+            if (v == k)
+                return k;
+
+            maxi = max(v, maxi);
+
+            p++;
+            i = ss + 1;
+            ss = i;
+            k++;
         }
 
         return maxi;
