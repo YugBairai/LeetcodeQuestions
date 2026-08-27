0ec7d407f69b10c7bb5fba3569cdf91393d64f8a
YugBairai <yugbairai200@gmail.com>
2026-08-27 10:43:55 +0530

    Add Medium C++ Solution for 4020. Lexicographically Smallest Permutation Greater Than Target from LeetCode

--- LeetCode/Patterns/Greedy/4020. Lexicographically Smallest Permutation Greater Than Target/Solution.cpp
+++ LeetCode/Patterns/Greedy/4020. Lexicographically Smallest Permutation Greater Than Target/Solution.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
+    string ans = "";
+
+    void solve(string &s, int index, string target) {
+
+        if (index == s.size()) {
+
+            if (s > target) {
+                if (ans == "" || s < ans) {
+                    ans = s;
+                }
+            }
+
+            return;
+        }
+
+        for (int i = index; i < s.size(); i++) {
+
+            swap(s[index], s[i]);
+
+            solve(s, index + 1, target);
+
+            swap(s[index], s[i]);
+        }
+    }
+
     string lexGreaterPermutation(string s, string target) {
 
-        vector<int> count(26, 0);
-        for (char ch : s) {
-            count[ch - 'a']++;
-        }
+        solve(s, 0, target);
 
-        int n = s.size();
-        int matched = 0;
-
-        while (matched < n && count[target[matched] - 'a'] > 0) {
-            count[target[matched] - 'a']--;
-            matched++;
-        }
-
-        int start = (matched < n ? matched : n - 1);
-
-        for (int i = start; i >= 0; i--) {
-            if (i < matched) {
-                count[target[i] - 'a']++;
-            }
-
-            int bigger = -1;
-            for (int ch = target[i] - 'a' + 1; ch < 26; ch++) {
-                if (count[ch] > 0) {
-                    bigger = ch;
-                    break;
-                }
-            }
-
-            if (bigger != -1) {
-                count[bigger]--;
-
-                string answer = target.substr(0, i);
-
-                answer += char('a' + bigger);
-
-                for (int ch = 0; ch < 26; ch++) {
-                    answer.append(count[ch], char('a' + ch));
-                }
-
-                return answer;
-            }
-        }
-
-        return "";
+        return ans;
     }
 };
