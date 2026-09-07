b162fd353fc5bbc9c2489967418ac9d725345436
YugBairai <yugbairai200@gmail.com>
2026-09-07 20:44:35 +0530

    Add Medium C++ Solution for 209. Minimum Size Subarray Sum from LeetCode

---
+++ LeetCode/Patterns/Sliding Window/209. Minimum Size Subarray Sum/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+
+        int l=0;
+        int sum = 0;
+        int ans = INT_MAX;
+
+        for(int i=0; i<nums.size(); i++){
+
+            sum += nums[i];
+
+            while (sum >= target) {
+
+                ans = min(ans, i - l + 1);
+
+                sum -= nums[l];
+                l++;
+            }
+        }
+
+        if(ans==INT_MAX) return 0;
+        else return ans;
+    }
+};
