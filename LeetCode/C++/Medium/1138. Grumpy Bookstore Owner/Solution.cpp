79c8576b762d786600a369001b48f245ea75e267
YugBairai <yugbairai200@gmail.com>
2026-09-09 10:23:30 +0530

    Add Medium C++ Solution for 1138. Grumpy Bookstore Owner from LeetCode

--- LeetCode/C++/Medium/1138. Grumpy Bookstore Owner/Solution.cpp
+++ LeetCode/C++/Medium/1138. Grumpy Bookstore Owner/Solution.cpp
@@ -3,20 +3,38 @@ public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
 
         int n = customers.size();
-        int x = n-minutes;
 
-        int sum = 0;
+        int alreadySatisfied = 0;
 
-        for(int i=x; i<customers.size(); i++){
-            sum+=customers[i];
-        }
-
-        for(int i=0; i<x; i++){
-            if(grumpy[i]==0){
-                sum+=customers[i];
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0) {
+                alreadySatisfied += customers[i];
             }
         }
 
-        return sum;
+        int extra = 0;
+
+        for (int i = 0; i < minutes; i++) {
+            if (grumpy[i] == 1) {
+                extra += customers[i];
+            }
+        }
+
+        int maxExtra = extra;
+
+        for (int i = minutes; i < n; i++) {
+
+            if (grumpy[i] == 1) {
+                extra += customers[i];
+            }
+
+            if (grumpy[i - minutes] == 1) {
+                extra -= customers[i - minutes];
+            }
+
+            maxExtra = max(maxExtra, extra);
+        }
+
+        return alreadySatisfied + maxExtra;
     }
 };
